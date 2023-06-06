27e603d751600ad0e7627466b65d88822c75c8d9
Daniel-105 <danielpinto105@gmail.com>
2023-06-06 16:45:33 +0100

    uploaded text based calculator

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿
+
+
+
+
+
+Console.Write("Insert the first number: ");
+int firstNum = int.Parse(Console.ReadLine());
+
+Console.Write("Insert the operation (plus, minus, multiply and divide) ");
+var operation = Console.ReadLine();
+
+Console.Write("Insert the second number: ");
+int secondNum = int.Parse(Console.ReadLine());
+
+ if (operation == "plus")
+    {
+        Console.WriteLine(firstNum + secondNum);
+    }
+    else if (operation == "minus")
+    {
+        Console.WriteLine(firstNum - secondNum);
+    }
+      else if (operation == "multiply")
+    {
+        Console.WriteLine(firstNum * secondNum);
+    }
+      else if (operation == "divide")
+    {
+        Console.WriteLine(firstNum / secondNum);
+    }
+
